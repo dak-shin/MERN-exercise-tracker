4754d782cd176a7e3fcf3e867d210171adbbce46
Dakshin S.A <dakshinsa2107@gmail.com>
2021-04-13 01:50:13 +0530

    create user and exercise added(only frontend)

--- src/components/createuser.js
+++ src/components/createuser.js
@@ -1,10 +1,47 @@
 import React from 'react';
-
+import {useState} from 'react'
 const CreateUser = () => {
+    
+    const [user, setUser] = useState({
+        username:''
+    });
+
+    const handleChange = (event) => {
+
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setUser({...user, [name]:value});
+
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(user);
+        setUser({
+            username:''
+        });
+    }
+
     return(
 
-        <div>
-            <h4>This the create User page</h4>
+        <div className="form">
+            <form action="/">
+                <div className="form-group">
+                    <label htmlFor="username">Username : </label>
+                    <input 
+                    type="text" 
+                    name="username"
+                    id="username"
+                    value={user.username}
+                    onChange={handleChange} />
+                </div>
+
+                <br/>
+                <button type="submit" onClick={handleSubmit}>
+                    Submit
+                </button>
+            </form>
         </div>
 
     );
